e136b9d57ae40713af6bb8cc262f5bbae9e9cc04
Jesus <jesus.test09@gmail.com>
2020-06-18 23:46:26 -0500

    Se agraga la clase Persona

---
+++ CrudMVC5Persona/CrudMVC5Persona/Models/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudMVC5Persona.Models
+{
+    public class Persona
+    {
+        public int ID_PERSONA { get; set; }
+        public string NOMBRES { get; set; }
+        public string APELLIDOP { get; set; }
+        public string APELLIDOM { get; set; }
+        public int EDAD { get; set; }
+        public string TELEFONO { get; set; }
+        public string CORREO { get; set; }
+        public string DIRECCION { get; set; }
+
+        public string SEXO { get; set; }
+
+
+    }
+}
